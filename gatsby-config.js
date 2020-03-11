c9ab4b9e2a272e8a614a05811e570922853e5c4f
Oscar <oscaryih@hotmail.com>
2020-03-11 14:30:52 -0700

    Add google analytics and reduze background.jpg size

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,17 @@ module.exports = {
       'We specialize in developing and designing ecommerce websites and personal/business websites. We deal with technology and development so you can focus on your business worry-free.',
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-156563309-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        anonymize: true,
+        respectDNT: true,
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
